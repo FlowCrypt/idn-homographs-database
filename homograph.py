3a71449ed3dd7d6302d9c9c35468e4e9a6563b88
Alex Alvarado <alej.alvarado94@gmail.com>
2020-06-19 06:49:56 -0500

    Revert "remove whois dependency"
    This reverts commit e4f2a597147cf342174449c1312af54e12d7565c.

--- homograph.py
+++ homograph.py
@@ -1,5 +1,6 @@
 import json
 import string
+import whois
 
 # The simchar data from ShamFinder could be structured
 # waaay more efficiently & less redundantly
@@ -25,10 +26,10 @@ def homographic(domain1, domain2):
     return False
 
   for letter1, letter2 in zip(domain1, domain2):
-
+    
     if not homoglyphic(letter1, letter2):
       return False
-
+      
   return True
 
 # Get all homoglyphs of a character
