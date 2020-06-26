392c8113918143b257d3f0d4f9dd6777e0f662a0
Alex Alvarado <alej.alvarado94@gmail.com>
2020-06-25 19:23:23 -0500

    change minor version for PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'homograph',
   packages = ['homograph'],
-  version = '0.01',
+  version = '0.02',
   license='MIT',
   description = 'IDN Homograph detection tools',
   author = 'Alex Alvarado',
@@ -11,8 +11,9 @@ setup(
   # Note: The below release doesn't exist yet!
   download_url = 'https://github.com/FlowCrypt/idn-homographs-database/archive/0.02.tar.gz',
   keywords = ['IDN', 'homograph', 'attack', 'security'],
-  install_requires=[],
-  classifiers=[
+  install_requires = [],
+  include_package_data = True,
+  classifiers = [
     'Development Status :: 1 - Planning',
     'Intended Audience :: Developers',
     'Topic :: Security',
