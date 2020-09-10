694720f58210c454f9aed4a2e2d2706b519a3eeb
seisvelas <alej.alvarado94@gmail.com>
2020-09-09 23:47:18 -0500

    Fix UTF bug

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from distutils.core import setup
 setup(
   name = 'homograph',
   packages = ['homograph'],
-  version = '0.02',
+  version = '1.2',
   license='MIT',
   description = 'IDN Homograph detection tools',
   author = 'Alex Alvarado',
   author_email = 'alex@flowcrypt.com',
-  url = 'https://github.com/flowcrypt/idn-homograph-database',
-  # Note: The below release doesn't exist yet!
-  download_url = 'https://github.com/FlowCrypt/idn-homographs-database/archive/0.02.tar.gz',
+  url = 'https://github.com/flowcrypt/idn-homographs-database',
+  # Note: The below release doesn't exist until after this code is merged
+  download_url = 'https://github.com/flowcrypt/idn-homographs-database/archive/1.2.tar.gz',
   keywords = ['IDN', 'homograph', 'attack', 'security'],
   install_requires = [],
   include_package_data = True,
+  package_data={'homograph': ['homographs.json']},
   classifiers = [
     'Development Status :: 1 - Planning',
     'Intended Audience :: Developers',
